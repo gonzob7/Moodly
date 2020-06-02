b1bdc008e1b629f657cb5778512b2794ec17b4b6
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-06-02 13:20:46 -0700

    started network manager

---
+++ Moodly/Managers/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  Moodly
+//
+//  Created by Gonzalo Birrueta on 6/2/20.
+//  Copyright © 2020 Gonzalo Birrueta. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager{
+    static let shared = NetworkManager()
+    
+    
+    
+    
+    
+}
---
+++ Moodly/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Moodly
+//
+//  Created by Gonzalo Birrueta on 6/2/20.
+//  Copyright © 2020 Gonzalo Birrueta. All rights reserved.
+//
+
+import Foundation
+
+enum Constants{
+    static let apiKey = "test123"
+}
