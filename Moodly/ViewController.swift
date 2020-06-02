34f92c496f58f16c00a76982c27e7eb595eca68d
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-06-02 12:46:40 -0700

    Initial Commit

---
+++ Moodly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moodly
+//
+//  Created by Gonzalo Birrueta on 6/2/20.
+//  Copyright © 2020 Gonzalo Birrueta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
